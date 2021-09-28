7941cb955a3c62a3bf8ba6898cd97f55d499b16c
deepak5998 <deepakkumbhar1920@gmail.com>
2021-09-28 17:27:49 +0530

    daywise

--- Day 5/src/LeapYear.java
+++ Day 5/src/LeapYear.java
@@ -9,7 +9,7 @@ public class LeapYear {
         if ((year % 4) == 0)
             if((year % 100) == 0)
                 if((year % 400) == 0)
-                    System.out.println("yes");
+                    System.out.println("yes,Leap year");
                 else
                 System.out.println("no");
             else
